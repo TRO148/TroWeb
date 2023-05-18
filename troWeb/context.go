b6419243ee109b9b6d4dde31eb428bf3a01629fe
trotro <1481895142@qq.com>
2023-05-18 10:39:03 +0800

    context

---
+++ troWeb/context.go
@@ -0,0 +1,75 @@
+package troWeb
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// Context 包装
+type Context struct {
+	// 原生的参数
+	Writer http.ResponseWriter
+	Req    *http.Request
+
+	// 请求信息
+	Path   string
+	Method string
+
+	// 响应信息
+	StatusCode int
+}
+
+// PostForm 获取路由参数
+func (c *Context) PostForm(key string) string {
+	return c.Req.FormValue(key)
+}
+
+// Query 获取查询参数
+func (c *Context) Query(key string) string {
+	return c.Req.URL.Query().Get(key)
+}
+
+// Status 设置响应状态码
+func (c *Context) Status(code int) {
+	c.StatusCode = code
+	c.Writer.WriteHeader(code)
+}
+
+// SetHeader 设置响应头
+func (c *Context) SetHeader(key string, value string) {
+	c.Writer.Header().Set(key, value)
+}
+
+// 多种响应体写入方式
+
+// String 设置响应体
+func (c *Context) String(code int, format string, values ...interface{}) {
+	c.SetHeader("Content-Type", "text/plain")
+	c.Status(code)
+	c.Writer.Write([]byte(fmt.Sprintf(format, values...)))
+}
+
+// JSON 设置响应体
+func (c *Context) JSON(code int, obj interface{}) {
+	c.SetHeader("Content-Type", "application/json")
+	c.Status(code)
+	// 将obj序列化为json格式
+	encoder := json.NewEncoder(c.Writer)
+	if err := encoder.Encode(obj); err != nil {
+		http.Error(c.Writer, err.Error(), 500)
+	}
+}
+
+// Data 设置响应体
+func (c *Context) Data(code int, data []byte) {
+	c.Status(code)
+	c.Writer.Write(data)
+}
+
+// HTML 设置响应体
+func (c *Context) HTML(code int, html string) {
+	c.SetHeader("Content-Type", "text/html")
+	c.Status(code)
+	c.Writer.Write([]byte(html))
+}
